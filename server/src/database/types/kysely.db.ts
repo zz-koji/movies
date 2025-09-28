5f3e0893976b0a36cd05894eadbfd71121a82f5c
Koji <koji.init@gmail.com>
2025-09-28 23:45:38 +0000

    added local_movies to kysely db

--- server/src/database/types/kysely.db.ts
+++ server/src/database/types/kysely.db.ts
@@ -1,7 +1,9 @@
 import { MovieRequest as MovieRequestTable } from "src/movie-requests/types"
+import { LocalMovie as LocalMoviesTable } from "src/movies/types"
 import { UsersTable } from "src/users/types"
 
 export type Database = {
+	local_movies: LocalMoviesTable
 	movie_requests: MovieRequestTable
 	users: UsersTable
 }
