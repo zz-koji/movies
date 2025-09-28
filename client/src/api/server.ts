d9ebb580ee45244c61b32367f4f80d0e9a63b7a1
Koji <koji.init@gmail.com>
2025-09-28 03:44:42 +0000

    final docker touches

--- client/src/api/server.ts
+++ client/src/api/server.ts
@@ -1,4 +1,4 @@
-const VITE_PUBLIC_API_BASE_URL = import.meta.env.VITE_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+const VITE_PUBLIC_API_BASE_URL = import.meta.env.VITE_PUBLIC_API_BASE_URL || '/api';
 
 export type GetMoviesRequest = {
 	title: string;
